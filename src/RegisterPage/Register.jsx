643e80b747c38537491182c0db5c949cf560d242
codedsholly11 <codedsholly@gmail.com>
2024-08-03 12:47:26 +0100

    did register

--- src/RegisterPage/Register.jsx
+++ src/RegisterPage/Register.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-         <div className='flex flex-col items-center justify-center gap-4'>
+    <div className='px-[40px]'>
+         <div className='flex flex-col items-center justify-center gap-4 py-[30px]'>
             <h1 className='text-4xl text-red-400'>Web3 Lagos Conference 3.0: Registration Form</h1>
             <div className='flex flex-col items-center justify-center'>
                 <h2 className='text-3xl text-blue-600'>Register Now!</h2>
@@ -13,6 +13,72 @@ const Register = () => {
         </div>
         <div>
             <p className='border-b-2'></p>
+        </div>
+        <div>
+            <h2 className='text-3xl font-bold py-[40px]'>Personal Details</h2>
+            <div className='px-[40px] py-[20px] border-2 rounded-xl flex flex-col gap-5'>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Name</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='Put in your fullname' />
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Email Address</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='Enter your email' />
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Phone Number</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='Put in your phone number'/>
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Country</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='the country you are coming from'/>
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Location</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='e.g lagos'/>
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Telegrame Username</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='Put in your telegrame ID' name="" id="" />
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>X Handle</p>
+                    <input className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]' type="text" placeholder='Put in your xhandle' name="" id="" />
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>Gender</p>
+                    <select className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]'>
+                        <option value="">pleas select an option</option>
+                        <option value="">Male</option>
+                        <option value="">Female</option>
+                        <option value="">Others</option>
+                    </select>
+                </div>
+                <div className='flex flex-col gap-3'>
+                    <p className='font-bold'>What Best Describes Your Role In Web3</p>
+                    <select className='border-2 w-[86vw] h-[8vh] rounded-lg px-[15px]'>
+                                <option value="">Select your role</option>
+                                <option value="">Developer</option>
+                                <option value="">Investor</option>
+                                <option value="">Community Manager/Community Builder</option>
+                                <option value="">Traders</option>
+                                <option value="">Newbies</option>
+                                <option value="">Desighner</option>
+                                <option value="">Marketer</option>
+                                <option value="">Product Manager</option>
+                                <option value="">Content</option>
+                                <option value="">Researchers</option>
+                                <option value="">others</option>
+                    </select>
+                    
+                </div>
+                
+                <div className='flex justify-between py-[30px]'>
+                    <button>Clear form</button>
+                    <button className='bg-blue-700 px-[20px] py-[6px] rounded-lg text-white '>sumit</button>
+                </div>
+                
+            </div>
         </div>
       
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const Nav = () => {
+  const [menu, setMenu] =useState(false)
+  function handleMenu(){
+    setMenu(!menu)
+  }
+
   return (
-    <div className='flex items-center justify-between py-[10px] px-[70px] '>
-        <ul className='flex gap-[20px]'>
-            <li className='text-2xl'><Link to='/'>Home</Link></li>
+    <div className='flex items-center justify-between py-[10px] md:px-[70px] px-[25px]'>
+        <ul className='md:flex gap-[20px] hidden'>
+            <li className='md:text-2xl'><Link to='/'>Home</Link></li>
             <li className='text-2xl cursor-pointer'><Link to='/Insighter'>Insighter</Link></li>
             <li className='text-2xl cursor-pointer'><Link to='/Hack'>Hackathoon</Link></li>
         </ul>
-        <div className='flex gap-[20px]'>
-            <button className='border-2 border-black rounded-lg h-[8vh] w-[16vw] text-xl'><Link to='/Speaker'>Register as a Speaker</Link></button>
-            <button className='border-2 border-black rounded-lg h-[8vh] w-[16vw] text-2xl bg-black text-white'>Register Here</button>
+        <div>
+            <button onClick={handleMenu} className='flex md: text-5xl md:hidden'>{menu ?<IoClose/>:<IoMenu/>}</button>
         </div>
-      
+        <div className='flex gap-[20px]'>
+            <button className='border-2 border-black rounded-lg h-[8vh] md:w-[16vw] text-xl hidden md:flex md:justify-center md:items-center'><Link to='/Speaker'>Register as a Speaker</Link></button>
+            <button className='border-2 border-black rounded-lg h-[8vh] md:w-[16vw] text-2xl bg-black text-white px-[20px]'><Link to='/Reg'>Register Here</Link></button>
+        </div>
+     
+      {menu&&
+        <ul className={`flex gap-[10px] w-[85%] text-xl bg-green-200 flex-col items-center py-[10px] font-bold top-0 absolute ${menu ?'top-14': '-top-full'} `}>
+        <li className='text-3xl'><Link to='/'>Home</Link></li>
+        <li className='text-3xl cursor-pointer'><Link to='/Insighter'>Insighter</Link></li>
+        <li className='text-3xl cursor-pointer'><Link to='/Hack'>Hackathoon</Link></li>
+        <button className='border-2 border-black rounded-lg h-[8vh] md:w-[16vw] text-xl md:flex md:item-center px-[20px]'><Link to='/Speaker'>Register as a Speaker</Link></button>
+        </ul>
+        
+      }
     </div>
   )
 }
